33b50d7b440cedd93174e1ed3c645b2973af9c95
hubert dulay <hubert.dulay@gmail.com>
2020-12-05 01:06:16 -0500

    adding sources

--- src/main/java/com/github/hubert/swift/App.java
+++ src/main/java/com/github/hubert/swift/App.java
@@ -1,13 +1,109 @@
 package com.github.hubert.swift;
 
+import com.prowidesoftware.swift.model.field.Field;
+import com.prowidesoftware.swift.model.field.Field20;
+import com.prowidesoftware.swift.model.field.Field32A;
+import com.prowidesoftware.swift.model.mt.mt1xx.MT103;
+import com.prowidesoftware.swift.utils.Lib;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
 /**
- * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "swift-parser-example");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "swift-parser-example");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+
+        KStream<String, GenericRecord> swiftMessages = builder.stream("ibmmq"); // input topic
+
+        KStream<String, GenericRecord> data = swiftMessages.mapValues((k, v) -> {
+
+            String raw = v.get("text").toString();
+
+            /*
+             * Read and parse the file content into a SWIFT message object
+             * Parse from File could also be used here
+             */
+            try(ByteArrayInputStream bais = new ByteArrayInputStream(raw.getBytes())) {
+                MT103 mt = MT103.parse(Lib.readStream(bais, null));
+                GenericRecord mt103 = getGenericRecord(mt, raw);
+                return mt103;
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                // you should create an error avro message and populate it with error details
+                // and the original message and return it. you can "branch" it to route these
+                // messages to an error topic.
+                return v;
+            }
+        });
+
+        data.print(Printed.toSysOut());
+
+        KStream<String, GenericRecord>[] branches = data.branch(
+                (key, value) -> (value.get("raw") == null), // missing the raw field, then must be error
+                (key, value) -> true
+        );
+
+        branches[0].to("errors"); // output topic
+        branches[1].to("mt103"); // output topic
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+
+        streams.setUncaughtExceptionHandler((t, e) -> {
+            e.printStackTrace();
+        });
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    private static GenericRecord getGenericRecord(MT103 mt, String raw) throws IOException {
+        try(InputStream is = App.class.getClassLoader().getResourceAsStream("mt103.avsc")) {
+            String schemaStr = new String(IOUtils.toByteArray(is));
+
+            Schema.Parser parser = new Schema.Parser();
+            Schema schema = parser.parse(schemaStr);
+            GenericRecord avroRecord = new GenericData.Record(schema);
+            avroRecord.put("sender", mt.getSender());
+            avroRecord.put("receiver", mt.getReceiver());
+
+            Field20 ref = mt.getField20();
+            avroRecord.put("Field20", Field.getLabel(ref.getName(), mt.getMessageType(), null) + ": " +
+                    ref.getComponent(Field20.REFERENCE));
+
+            Field32A f = mt.getField32A();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+            avroRecord.put("value_date", f.getDateAsCalendar().getTimeInMillis());
+
+            avroRecord.put("amount", f.getCurrency()+" "+f.getAmount());
+
+            avroRecord.put("raw", raw);
+            return avroRecord;
+        }
     }
 }
